56bf0532eee59c4274f21b800bda7399ff56f3da
nwankwohenryugo-hue <nwankwo.henry.ugo@gmail.com>
2026-01-04 22:57:04 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+DB_CONFIG = {
+    'host':     'localhost',
+    'user':     'root',          # change if needed
+    'password': 'your_password', # ← NEVER push real password!
+    'database': 'feedback_db'
+}
